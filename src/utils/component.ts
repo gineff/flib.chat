b4372b610ed9a70a1016d395699c679326d61fb9
gineff <canone@inbox.ru>
2022-12-03 13:58:57 +0300

    fix

--- src/utils/component.ts
+++ src/utils/component.ts
@@ -87,7 +87,7 @@ export default class Component<P = unknown> {
     for (const key in props) {
       const value: unknown = props[key];
 
-      if (value && "isComponent" in value) {
+      if (typeof value === "object" && value !== null && "isComponent" in value) {
         registerComponent(key, value as unknown as typeof Component);
       } else if (key === "template") {
         this.template = value as string;
@@ -270,7 +270,9 @@ export default class Component<P = unknown> {
 
   proxyStateOnce() {
     this.state = this._makePropsProxy(this.state);
-    this.proxyStateOnce = (): void => {""};
+    this.proxyStateOnce = (): void => {
+      ("");
+    };
   }
 
   addEventHandler(element: HTMLElement, props: any) {
